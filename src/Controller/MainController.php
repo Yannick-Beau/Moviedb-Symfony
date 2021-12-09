8b7ad27d8a3238730a815187dae0abfb75e27361
Yannick-Beau <beauyannick@gmail.com>
2021-12-09 18:49:01 +0100

    add route movie_show

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
+    /**
+     * Liste les films
+     */
     #[Route('/', name: 'home')]
     public function home(MovieRepository $movieRepository): Response
     {
@@ -16,9 +20,25 @@ class MainController extends AbstractController
             [],
             ['title' => 'ASC']
         );
-        
+
         return $this->render('main/home.html.twig', [
             'movies' => $movies,
         ]);
     }
+    /**
+     * Affiche un film
+     */
+    #[Route('/movie/{id<\d+>}', name: 'movie_show')]
+    public function movie(Movie $movie): Response
+    {
+       if ($movie === null) {
+           throw $this->createNotFoundException('Film non trouvé.');
+       }
+
+        return $this->render('main/movie_show.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+
+
 }
