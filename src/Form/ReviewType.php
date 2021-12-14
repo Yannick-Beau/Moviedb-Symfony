46c099b2ad80fb993c990c9e151cdf5206278dc7
Yannick-Beau <beauyannick@gmail.com>
2021-12-14 19:55:46 +0100

    modification option form add review

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -25,11 +26,34 @@ class ReviewType extends AbstractType
             ->add('content', TextareaType::class, [
                 'label' => 'Critique',
             ])
-            ->add('rating', null, [
-                'label' => 'Apréciation (note sur 5)'
+            ->add('rating', ChoiceType::class, [
+                'label' => 'Apréciation',
+                'choices' => [
+                    // Label => valeur
+                    'Execellent' => 5,
+                    'Trés bon' => 4,
+                    'Bon' => 3,
+                    'Peut mieux faire' => 2,
+                    'A éviter' => 1,
+                ],
+                'placeholder' => 'Choisissez une apréciation',
+                // Multiple et expanded permet de choisir la façon d'afficher les choix
+                // Plusieur choix possibles ou non
+                'multiple' => false,
+                // Chaque choix à son widget html ou non
+                'expanded' => false,
             ])
-            ->add('reactions', null, [
-                'label' => 'Ce film vous a fait'
+            ->add('reactions', ChoiceType::class, [
+                'label' => 'Ce film vous a fait',
+                'choices' => [
+                    'Rire' => 'smile',
+                    'Pleurer' => 'cry',
+                    'Réfléchir' => 'think',
+                    'Dormir' => 'sleep',
+                    'Rêver' => 'dream',
+                ],
+                'multiple' => true,
+                'expanded' => true,
             ])
             ->add('watchedAt', null, [
                 'label' => 'Vous avez vu ce film le'
